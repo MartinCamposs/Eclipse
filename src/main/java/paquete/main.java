c8835611221c920f71533b58d275d74edda6b128
MartinCamposs <martincamposlopez4@gmail.com>
2023-09-18 10:13:50 +0200

    Primer commit

---
+++ src/main/java/paquete/main.java
@@ -0,0 +1,11 @@
+package paquete;
+
+import java.util.*;
+public class main {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("ACDA");
+
+	}
+}
